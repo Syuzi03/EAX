d610013b62cd294fcf4023cf3ea8ab2c1e683c97
Syuzi Gevorgyan <gevorgyansyuzi77@gmail.com>
2024-08-28 15:26:00 +0400

    added classwork

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 const bodyParser = require('body-parser');
 
-const client = new MongoClient('mongodb://localhost:27017');
+const client = new MongoClient('mongodb://127.0.0.1:27017');
 
 
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/products', async (req, res) => {
     try {
         await client.connect();
-        const db = client.db('eax1');
+        const db = client.db('syuzidb');
         const collection = db.collection('products');
         const { name, price } = req.body;
         const result = await collection.insertOne({name, price});
@@ -34,8 +35,8 @@ app.get('/products', async (req, res) => {
     }
 });
 
-const DEFAULT_PORT_VALUE = 3000;
-const PORT = process.env.PORT || DEFAULT_PORT_VALUE;
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
